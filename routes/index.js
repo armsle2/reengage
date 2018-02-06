a2abfbd00ed2b3b8832c2acf47b561af8ef0534a
LA The Fly Rebel <flyrebel@server.theflyrebel.com>
2018-02-06 10:02:47 -0500

    fixed api route

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,8 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
-// If no API routes are hit, send the React app
-// router.get('*', function(req, res) {
-  console.log(path.join(__dirname, "../client/build/index.html"));
-// });
+router.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
 module.exports = router;
