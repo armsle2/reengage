a6a5e710cc286ed6d439fcf0c3b856c7893eff33
andrejarboe <andrejarboe@gmail.com>
2018-02-06 11:07:29 -0500

    add user model

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  userName: {
+    type: String,
+    unique: true
+  },
+  // `email` must be of type String
+  // `email` must be unique
+  // `email` must match the regex pattern below and throws a custom error message if it does not
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    match: [/.+\@.+\..+/, "Please enter a valid e-mail address"]
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: "Password is Required",
+    validate: [
+      function(input) {
+        return input.length >= 6;
+      },
+      "Password should be longer."
+    ]
+  },
+  // `date` must be of type Date. The default value is the current date
+  userCreated: {
+    type: Date,
+    default: Date.now
+  },
+  //rewards are defaulted to completed false and 
+  rewards: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Survey",
+      completed: false,
+      used: false
+    }
+  ]
+});
+
+// This creates our model from the above schema, using mongoose's model method
+const User = mongoose.model("User", UserSchema);
+
+// Export the User model
+module.exports = User;
