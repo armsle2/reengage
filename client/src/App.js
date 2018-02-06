e72a2024c23503a44818f57b5ebf3b1a3cb4ca22
LA The Fly Rebel <flyrebel@server.theflyrebel.com>
2018-02-05 23:58:51 -0500

    replaced missing files

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  componentDidMount(){
+    console.log('this is working')
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Landing</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
