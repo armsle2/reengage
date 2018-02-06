7e51ca2d65cab1a74c35c9666f898a6c6658b8d3
LA The Fly Rebel <flyrebel@server.theflyrebel.com>
2018-02-06 09:50:38 -0500

    redid build folder

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(routes);
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-
+  
 
 app.listen(PORT, () => {console.log(`listening to port ${PORT}`)})
 
