42ad23a3c6f813ebcf531f4f7f71bfb1938193a3
john liscar <john.liscar@gmail.com>
2018-02-07 13:53:03 -0500

    changed route names

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,72 +5,72 @@ export default {
 //Customers
   // Gets all users
   getCustomers: function() {
-    return axios.get("/api/customers");
+    return axios.get("/api/customers-api");
   },
   // Gets the customer with the given id
   getCustomer: function(id) {
-    return axios.get("/api/customers/" + id);
+    return axios.get("/api/customers-api/" + id);
   },
   // Deletes the customer with the given id
   deleteCustomer: function(id) {
-    return axios.delete("/api/customers/" + id);
+    return axios.delete("/api/customers-api/" + id);
   },
   // Saves a customer to the database
   saveCustomer: function(customerData) {
-    return axios.post("/api/customers", customerData);
+    return axios.post("/api/customers-api", customerData);
   },
 
 //Businesses
   // Gets all businesses
-  getBusinesses: function() {
-    return axios.get("/api/businesses");
+  getCompanies: function() {
+    return axios.get("/api/companies-api");
   },
   // Gets the customer with the given id
-  getBusiness: function(id) {
-    return axios.get("/api/business/" + id);
+  getCompany: function(id) {
+    return axios.get("/api/companies-api/" + id);
   },
   // Deletes the customer with the given id
-  deleteBusiness: function(id) {
-    return axios.delete("/api/businesses/" + id);
+  deleteCompany: function(id) {
+    return axios.delete("/api/companies-api/" + id);
   },
   // Saves a customer to the database
-  saveBusiness: function(businessData) {
-    return axios.post("/api/businesses", businessData);
+  saveCompany: function(companyData) {
+    return axios.post("/api/companies-api", companyData);
   },
 
 //Surveys
   // Gets all surveys
   getSurveys: function() {
-    return axios.get("/api/surveys");
+    return axios.get("/api/surveys-api");
   },
   // Gets the survey with the given id
   getSurvey: function(id) {
-    return axios.get("/api/surveys/" + id);
+    return axios.get("/api/surveys-api/" + id);
   },
   // Deletes the customer with the given id
   deleteSurvey: function(id) {
-    return axios.delete("/api/surveys/" + id);
+    return axios.delete("/api/surveys-api/" + id);
   },
   // Saves a customer to the database
   saveSurvey: function(surveyData) {
-    return axios.post("/api/surveys", surveyData);
+    return axios.post("/api/surveys-api", surveyData);
   },
 
 //Rewards
   // Gets all rewards
   getRewards: function() {
-    return axios.get("/api/rewards");
+    return axios.get("/api/rewards-api");
   },
   // Gets the survey with the given id
   getReward: function(id) {
-    return axios.get("/api/rewards/" + id);
+    return axios.get("/api/rewards-api/" + id);
   },
   // Deletes the customer with the given id
   deleteReward: function(id) {
-    return axios.delete("/api/rewards/" + id);
+    return axios.delete("/api/rewards-api/" + id);
   },
   // Saves a customer to the database
-  saveRewards: function(rewardData) {
-    return axios.post("/api/rewards", rewardData);
+  saveReward: function(rewardData) {
+    return axios.post("/api/rewards-api", rewardData);
   }
 };
