0da5078d56dd486a093d1cbde5f86be557faae9c
Leaveil <leaveil.armstrong@gmail.com>
2018-02-15 13:06:18 -0500

    deleted unused code from client index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// import 'bootstrap/dist/css/bootstrap.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const FinalApp = () => (
